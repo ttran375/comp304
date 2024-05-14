bf3161d37ba7758055f1b4053906392f5047c558
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-14 01:40:29 +0000

    Tue, May 14, 2024, 1:40 AM +00:00

--- Main.kt
+++ Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val number1: Int = 259
     val number2: Byte = number1.toByte() // Conversion truncates to the least significant 8 bits
-    println("number1 = $number1")
-    println("number2 = $number2")
+    println("number1 = $number1") //number1 = 259
+    println("number2 = $number2") //number2 = 3
 }
