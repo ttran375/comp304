3f126c0fd7025f1d7cfe07981d69ddd080e67b86
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-14 01:21:39 +0000

    Tue, May 14, 2024, 1:21 AM +00:00

---
+++ Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val range: Byte = 112
+    println("$range")
+
+    val iValue = 1234
+}
--- main.kt
+++
@@ -1,9 +0,0 @@
-
-/**
-* You can edit, run, and share this code. * play.kotlinlang.org
-*/
-fun main() {
-    var language: String
-    var language = "French";
-    val score: Int = 95;
-}
