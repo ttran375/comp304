d68c8e84d8aa246b610a52d1c2596b3ea8919d88
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-14 01:46:33 +0000

    Tue, May 14, 2024, 1:46 AM +00:00

--- Main.kt
+++ Main.kt
@@ -1,6 +1,22 @@
 fun main(args: Array<String>) {
-    val number1: Int = 259
-    val number2: Byte = number1.toByte() // Conversion truncates to the least significant 8 bits
-    println("number1 = $number1") //number1 = 259
-    println("number2 = $number2") //number2 = 3
+    // val number1: Int = 259
+    // val number2: Byte = number1.toByte() // Conversion truncates to the least significant 8 bits
+    // println("number1 = $number1") //number1 = 259
+    // println("number2 = $number2") //number2 = 3
+
+    fun main(args: Array<String>) {
+        val a = -12
+        val b = 12
+    
+        // Use of greater than operator
+        val max = if (a > b) {
+            println("a is larger than b.")
+            a
+        } else {
+            println("b is larger than a.")
+            b
+        }
+    
+        println("max = $max")
+    }    
 }
