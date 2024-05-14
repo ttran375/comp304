8f9110f27a0cf891cbfe234d6657e576251a3045
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-14 00:58:46 +0000

    Tue, May 14, 2024, 12:58 AM +00:00

--- main.kt
+++ main.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
-    println("Hi Kotlin, how are you!")
+
+/**
+* You can edit, run, and share this code. * play.kotlinlang.org
+*/
+fun main() {
+    var name =
+"Yuva"
+var age = 5;
+println("Calling functions...")
+print(i)
+
 }
