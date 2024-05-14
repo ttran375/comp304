d1032fd3633274ee73cf6dc1994001dd74321a4e
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-14 01:09:55 +0000

    Tue, May 14, 2024, 1:09 AM +00:00

--- main.kt
+++ main.kt
@@ -3,10 +3,7 @@
 * You can edit, run, and share this code. * play.kotlinlang.org
 */
 fun main() {
-    var name =
-"Yuva"
-var age = 5;
-println("Calling functions...")
-print(i)
-
+    var language: String
+    var language = "French";
+    val score: Int = 95;
 }
