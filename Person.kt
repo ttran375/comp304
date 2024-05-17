a27782f09604b999bef9d7a0b892e815893b6095
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-17 23:44:31 +0000

    Fri, May 17, 2024, 11:44 PM +00:00

--- Person.kt
+++ Person.kt
@@ -1,13 +1,24 @@
 fun main(args: Array<String>) {
+    println("person1 is instantiated")
     val person1 = Person("joe", 25)
+    println("person2 is instantiated")
+    val person2 = Person("Jack")
+    println("person3 is instantiated")
+    val person3 = Person()
+
+    // Printing the values to show the output as mentioned
+    println("Person1: First Name = ${person1.firstName}, Age = ${person1.age}")
+    println("Person2: First Name = ${person2.firstName}, Age = ${person2.age}")
+    println("Person3: First Name = ${person3.firstName}, Age = ${person3.age}")
 }
 
-class Person(fName: String, personAge: Int) {
-    val firstName = fName.capitalize()
-    var age = personAge
+class Person(__firstName: String = "UNKNOWN", __age: Int = 0) {
+    val firstName = __firstName.capitalize()
+    var age = __age
 
+    // Initializer block
     init {
         println("First Name = $firstName")
-        println("Age = $age")
+        println("Age = $age\n")
     }
 }
