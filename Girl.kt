14c30733e171d1409e16ad233f268d4a6fcc1a0f
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-17 23:52:48 +0000

    Fri, May 17, 2024, 11:52 PM +00:00

---
+++ Girl.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    val maria = Girl()
+    maria.actualAge = 15
+    maria.age = 15
+    println("Maria: actual age = ${maria.actualAge}")
+    println("Maria: pretended age = ${maria.age}")
+
+    val angela = Girl()
+    angela.actualAge = 35
+    angela.age = 35
+    println("Angela: actual age = ${angela.actualAge}")
+    println("Angela: pretended age = ${angela.age}")
+}
+
+class Girl {
+    var actualAge: Int = 0
+
+    var age: Int = 0
+        get() = field
+        set(value) {
+            field = when {
+                value < 18 -> 18
+                value in 18..30 -> value
+                else -> value - 3
+            }
+        }
+}
--- Person.kt
+++ Person.kt
@@ -1,24 +1,17 @@
 fun main(args: Array<String>) {
-    println("person1 is instantiated")
-    val person1 = Person("joe", 25)
-    println("person2 is instantiated")
-    val person2 = Person("Jack")
-    println("person3 is instantiated")
-    val person3 = Person()
-
-    // Printing the values to show the output as mentioned
-    println("Person1: First Name = ${person1.firstName}, Age = ${person1.age}")
-    println("Person2: First Name = ${person2.firstName}, Age = ${person2.age}")
-    println("Person3: First Name = ${person3.firstName}, Age = ${person3.age}")
+    val person = Person()
+    println("Initial Name: ${person.name}")
+    
+    person.name = "Alice"
+    println("Updated Name: ${person.name}")
 }
 
-class Person(__firstName: String = "UNKNOWN", __age: Int = 0) {
-    val firstName = __firstName.capitalize()
-    var age = __age
-
-    // Initializer block
-    init {
-        println("First Name = $firstName")
-        println("Age = $age\n")
-    }
+class Person {
+    var name: String = "defaultValue"
+        // getter
+        get() = field
+        // setter
+        set(value) {
+            field = value
+        }
 }
