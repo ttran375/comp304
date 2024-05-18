f34b02e9ac89b1ab15a5a5cc6597fa5c7e0dddbb
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-18 00:04:22 +0000

    Sat, May 18, 2024, 12:04 AM +00:00

--- Person.kt
+++ Person.kt
@@ -1,17 +1,51 @@
-fun main(args: Array<String>) {
-    val person = Person()
-    println("Initial Name: ${person.name}")
-    
-    person.name = "Alice"
-    println("Updated Name: ${person.name}")
+open class Person(val age: Int) {
+    fun eat() {
+        println("Eating...")
+    }
+
+    fun talk() {
+        println("Talking...")
+    }
+
+    fun walk() {
+        println("Walking...")
+    }
 }
 
-class Person {
-    var name: String = "defaultValue"
-        // getter
-        get() = field
-        // setter
-        set(value) {
-            field = value
-        }
+class MathTeacher(age: Int) : Person(age) {
+    fun teachMath() {
+        println("Teaching math...")
+    }
+}
+
+class Footballer(age: Int) : Person(age) {
+    fun playFootball() {
+        println("Playing football...")
+    }
+}
+
+class Businessman(age: Int) : Person(age) {
+    fun doBusiness() {
+        println("Doing business...")
+    }
+}
+
+fun main() {
+    val mathTeacher = MathTeacher(30)
+    mathTeacher.eat()
+    mathTeacher.talk()
+    mathTeacher.walk()
+    mathTeacher.teachMath()
+
+    val footballer = Footballer(25)
+    footballer.eat()
+    footballer.talk()
+    footballer.walk()
+    footballer.playFootball()
+
+    val businessman = Businessman(40)
+    businessman.eat()
+    businessman.talk()
+    businessman.walk()
+    businessman.doBusiness()
 }
