547b6d4b617ee03fced3d0ff6fe18e741a5fb7c4
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-19 23:25:04 +0000

    Add Callbacks

---
+++ Week3/LifeCycleCallbacks/app/src/main/java/com/example/lifecyclecallbacks/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.lifecyclecallbacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+    }
+    //
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+}
